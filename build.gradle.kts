8f47285f1b36cab02650303e13e9a6ada1508f0f
Symon02 <simone.pietrogrande@gmail.com>
2026-04-14 11:22:16 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("log4j:log4j:1.2.17")
+    implementation("org.apache.spark:spark-core_2.13:4.1.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/example/myMethods.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+class myMethods {
+    public static Iterator<Tuple2<String,Long>> wordCountPerDoc(String document) {
+        String[] tokens = document.split(" ");
+        HashMap<String, Long> counts = new HashMap<>();
+        ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+        for (String token : tokens) {
+            counts.put(token, 1L + counts.getOrDefault(token, 0L));
+        }
+        for (Map.Entry<String, Long> e : counts.entrySet()) {
+            pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+        }
+        return pairs.iterator();
+    }
+    public static Iterator<Tuple2<String,Long>> gatherPairs(Tuple2<Integer,Iterable<Tuple2<String, Long>>> element) {
+        HashMap<String, Long> counts = new HashMap<>();
+        for (Tuple2<String, Long> c : element._2()) {
+            counts.put(c._1(), c._2() + counts.getOrDefault(c._1(), 0L));
+        }
+        ArrayList<Tuple2<String, Long>> pairs = new ArrayList<>();
+        for (Map.Entry<String, Long> e : counts.entrySet()) {
+            pairs.add(new Tuple2<>(e.getKey(), e.getValue()));
+        }
+        return pairs.iterator();
+    }
+}
